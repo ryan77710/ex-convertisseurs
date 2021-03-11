2839d384a206de02746812c893db33a6d935e7f1
lollia ryan <lolliaryan@ryan-ordi.local>
2021-03-11 06:11:22 +0100

    origine'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import rates from "./money";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const rate = rates.USD;
+
+  const handleValueChange = (event) => {
+    setValue2(0);
+    const value = event.target.value;
+    setValue(value);
+    setValue2(value * rate);
+  };
+
+  const handleValue2Change = (event) => {
+    const value = event.target.value;
+    setValue(0);
+    setValue2(value);
+    setValue(value / rate);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>💵 EUR to USD 💵</h1>
+        <div>
+          <input type="text" value={value} onChange={handleValueChange} />
+          <label>€</label>
+        </div>
+        <div>
+          <input type="text" value={value2} onChange={handleValue2Change} />
+          <label>$</label>
+        </div>
+      </div>
     </div>
   );
 }
